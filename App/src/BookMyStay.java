7419f77a81355346f33c6c279326546b30f89825
sushrith18 <katarisushrith@gmail.com>
2026-04-07 08:55:34 +0530

    Added Use Case 1 - Application Entry

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,25 @@
+/**
+ * Book My Stay App - Use Case 1
+ * Entry point of the Hotel Booking application.
+ * Prints welcome message and version details.
+ *
+ * @author Sushrith
+ * @version 1.0
+ */
+public class BookMyStay {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("Welcome to Book My Stay App");
+
+        // Application name and version
+        System.out.println("Hotel Booking System v1.0");
+
+        // Confirmation message
+        System.out.println("Application Started Successfully!");
+    }
+}
